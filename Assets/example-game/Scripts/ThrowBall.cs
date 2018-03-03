da36f1dff351765f01d475dd3763aa8a5391464a
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2018-03-03 10:53:45 -0500

    Reindented ThrowBall and fixed a warning of hidden inheritance

--- Assets/example-game/Scripts/ThrowBall.cs
+++ Assets/example-game/Scripts/ThrowBall.cs
@@ -1,30 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ThrowBall : MonoBehaviour {
+public class ThrowBall : MonoBehaviour
+{
+	public GameObject ball;
+	public float forceMultiplier;
+	public UnityEngine.UI.Text text;
+	Camera _camera;
 
-    public GameObject ball;
-    public float forceMultiplier;
-    public UnityEngine.UI.Text text;
-    Camera camera;
+	void Start ()
+	{
+		_camera = GetComponent<Camera> ();
+	}
 
-    void Start () {
-        camera = GetComponent<Camera>();
-    }
-	
-    void Update () {
-        text.text = "Throw Power: "+forceMultiplier;
+	void Update ()
+	{
+		text.text = "Throw Power: " + forceMultiplier;
 
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
-            forceMultiplier += forceMultiplier/100;
-        else if (Input.GetAxis("Mouse ScrollWheel") < 0)
-            forceMultiplier -= forceMultiplier/100;
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0) {
+			forceMultiplier += forceMultiplier / 100;
+		} else if (Input.GetAxis ("Mouse ScrollWheel") < 0) {
+			forceMultiplier -= forceMultiplier / 100;
+		}
 
-        if (Input.GetButtonDown("Fire1")) {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-            GameObject newBall = Instantiate(ball, ray.origin, Quaternion.Euler(Vector3.zero));
-            newBall.GetComponent<Rigidbody>().AddForce(ray.direction * forceMultiplier);
-        }
-    }
+		if (Input.GetButtonDown ("Fire1")) {
+			Ray ray = _camera.ScreenPointToRay (Input.mousePosition);
+			GameObject newBall = Instantiate (ball, ray.origin, Quaternion.Euler (Vector3.zero));
+			newBall.GetComponent<Rigidbody> ().AddForce (ray.direction * forceMultiplier);
+		}
+	}
 }
